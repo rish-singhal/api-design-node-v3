7749539a03e666310e4d6223aab2d9ec4665e67e
Rishabh Singhal <zzxy334@gmail.com>
2021-12-13 20:25:49 +0530

    Completed Lesson 2

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -2,4 +2,25 @@ import { Router } from 'express'
 
 const router = Router()
 
+router
+  .route('/')
+  .get((req, res) => {
+    res.send('GET /item')
+  })
+  .post((req, res) => {
+    res.send('POST /item')
+  })
+
+router
+  .route('/:id')
+  .get((req, res) => {
+    res.send('GET /item/', req.params.id)
+  })
+  .put((req, res) => {
+    res.send('PUT /item/', req.params.id)
+  })
+  .delete((req, res) => {
+    res.send('DELETE /item/', req.params.id)
+  })
+
 export default router
